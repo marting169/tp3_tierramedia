642696b9b395382cbe2633db43d62ebf6a490d31
marting169 <marting169@gmail.com>
2021-12-15 17:11:12 -0300

    crear servlets

---
+++ src/main/java/servicios/PromocionService.java
@@ -0,0 +1,54 @@
+package servicios;
+
+import java.util.List;
+
+import model.Promocion;
+//import persistencia.PromocionDAO;
+import persistencia.commons.DAOFactory;
+
+public class PromocionService {
+	public List<Promocion> list() {
+		return DAOFactory.getPromocionDAO().findAll();
+	}
+
+//	public Promocion create(int id, String name, double cost, double duration, int capacity) {
+//
+//		Promocion attraction = new Promocion(id, name, cost, duration, capacity);
+//
+//		if (attraction.isValid()) {
+//			AtraccionDAO attractionDAO = DAOFactory.getPromocionDAO();
+//			attractionDAO.insert(attraction);
+//		}
+//
+//		return attraction;
+//	}
+
+//	public Promocion update(int id, String name, double cost, double duration, int capacity) {
+//
+//		PromocionDAO attractionDAO = DAOFactory.getPromocionDAO();
+//		Promocion attraction = attractionDAO.find(id);
+//
+//		attraction.setNombreAtraccion(name);
+//		attraction.setCosto(cost);
+//		attraction.setTiempo(duration);
+//		attraction.setCupo(capacity);
+//
+//		if (attraction.isValid()) {
+//			attractionDAO.update(attraction);
+//		}
+//
+//		return attraction;
+//	}
+
+//	public void delete(int id) {
+//		//Promocion promocion = new Promocion(id, null, null, null, null);
+//
+//		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+//		promocionDAO.delete(promocion);
+//	}
+
+	public Promocion find(Integer id) throws Exception {
+		return DAOFactory.getPromocionDAO().find(id);
+	}
+
+}
--- src/main/webapp/navAdmin.jsp
+++ src/main/webapp/navAdmin.jsp
@@ -13,9 +13,9 @@
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item"><a class="nav-link" href="atracciones" >Atracciones</a>
 					</li>
-					<li class="nav-item"><a class="nav-link" href="promociones.jsp" target="_blank">Promociones</a>
+					<li class="nav-item"><a class="nav-link" href="promociones" target="_blank">Promociones</a>
 					</li>
-					<li class="nav-item"><a class="nav-link" href="usuarios.jsp" target="_blank">Usuarios</a>
+					<li class="nav-item"><a class="nav-link" href="usuarios" target="_blank">Usuarios</a>
 					</li>
 				</ul>
 			</div>
@@ -25,7 +25,7 @@
 					id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
 					User</button>
 				<ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-					<li><button class="dropdown-item" type="button"><a class="nav-link" href="logout.jsp">Salir</a></button></li>
+					<li><button class="dropdown-item" type="button"><a class="nav-link" href="logout">Salir</a></button></li>
 				</ul>
 			</div>
 		</div>
